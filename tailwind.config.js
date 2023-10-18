de8f28f45a449843b077eeff4520fb79122b68a4
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2023-10-17 22:43:15 -0500

    Defining style with Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      "orange-soft": "hsl(35, 77%, 62%)",
+      "red-soft": "hsl(5, 85%, 63%)",
+      "off-white": "",
+      "grayish-blue": " hsl(233, 8%, 79%)",
+      "dark-grayish-blue": "hsl(236, 13%, 42%)",
+      "very-dark-blue": "hsl(240, 100%, 5%)",
+    },
+    fontFamily:{
+
+    },
+    extend: {},
+  },
+  plugins: [],
+};
